172490b40f7c6d7dcac44e7ede8e1a61d9848e7a
David Durst <davidbdurst@gmail.com>
2019-01-01 15:00:06 -0500

    added installer from source and binary installer

--- src/multiplication.cpp
+++ src/multiplication.cpp
@@ -3,10 +3,13 @@
 //
 #include <fstream>
 #include <iostream>
+#include <stdio.h>
+#include "MatrixConfig.h"
 
 int main (int argc, char * argv[]) {
-    std::cout << "multiply ";
-    for (int arg_index = 0; arg_index < argc; arg_index++) {
+    fprintf(stdout, "%s Version %d.%d", argv[0], Matrix_VERSION_MAJOR,
+        Matrix_VERSION_MINOR);
+    for (int arg_index = 1; arg_index < argc; arg_index++) {
         std::cout << argv[arg_index] << " ";
     }
     std::cout << std::endl;
