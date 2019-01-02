b1c020a1ad6cbeca7184b36bdf4d644e7e6dc747
David Durst <davidbdurst@gmail.com>
2019-01-01 21:55:06 -0500

    gave better names to result matrices that are compared

--- python/matrix_generator.py
+++ python/matrix_generator.py
@@ -25,13 +25,13 @@ os.mkdir(output_dir)
 
 np.savetxt(output_dir + "/left_matrix.csv", left_matrix, delimiter=",", fmt="%d")
 np.savetxt(output_dir + "/right_matrix.csv", right_matrix, delimiter=",", fmt="%d")
-np.savetxt(output_dir + "/product_matrix.csv", product_matrix, delimiter=",", fmt="%d")
+np.savetxt(output_dir + "/numpy_product_matrix.csv", product_matrix, delimiter=",", fmt="%d")
 
 command_to_run = "{}/matrix_multiplication {} {} {} {} {} {}".format(
     path_to_binary,
     output_dir + "/left_matrix.csv",
     output_dir + "/right_matrix.csv",
-    output_dir + "/cxx_product_matrix.csv",
+    output_dir + "/cpp_product_matrix.csv",
     num_rows_left_matrix,
     num_cols_left_matrix,
     num_cols_right_matrix)
@@ -40,8 +40,8 @@ print(path_to_binary)
 print(command_to_run)
 os.system(command_to_run)
 
-if not filecmp.cmp(output_dir + "/product_matrix.csv",
-                   output_dir + "/cxx_product_matrix.csv"):
+if not filecmp.cmp(output_dir + "/numpy_product_matrix.csv",
+                   output_dir + "/cpp_product_matrix.csv"):
     raise ValueError("numpy and my implementation don't match")
 else:
     print("Success!")
